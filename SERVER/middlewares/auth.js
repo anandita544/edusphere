6ee6ea34a99becc24dd8085510459de20e90316d
Anandita Singh <ananditasingh544@gmail.com>
2024-06-10 15:52:27 +0530

    first commit

---
+++ SERVER/middlewares/auth.js
@@ -0,0 +1,97 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// auth
+exports.auth = async (req, res, next) => {
+  try {
+    // EXTRACT TOKEN
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    // if TOKEN MISSING
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "TOKEN IS MISSING",
+      });
+    }
+
+    // VERIFY TOKEN
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+      req.user = decode;
+    } catch (err) {
+      // VERIFICATION ISSUE
+      return res.status(401).json({
+        success: false,
+        message: "TOKEN IS INVALID",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "SOMETHING WENT WRONG WHILE VALIDATING THWE TOKEN",
+    });
+  }
+};
+
+// isStudent
+exports.isStudent = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "SORRY THIS IS A Student ONLY ROUTE",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "USER STUDENT ROLE CANT BE VERIFIED.....VERIFICATION FAILED",
+    });
+  }
+};
+
+// isInstructor
+exports.isInstructor = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "instructor") {
+      return res.status(401).json({
+        success: false,
+        message: "SORRY THIS IS A INSTRUCTOR ONLY ROUTE",
+      });
+    }
+    console.log("helo instrcutor");
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "USER INSTRUCTOR ROLE CANT BE VERIFIED.....VERIFICATION FAILED",
+      err: err.message,
+    });
+  }
+};
+
+// isAdmin
+exports.isAdmin = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "admin") {
+      return res.status(401).json({
+        success: false,
+        message: "SORRY THIS IS AN ADMIN ONLY ROUTE",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "USER ADMIN ROLE CANT BE VERIFIED.....VERIFICATION FAILED",
+      err: err.message,
+    });
+  }
+};
---
+++ src/utils/constants.jsx
@@ -0,0 +1,10 @@
+export const ACCOUNT_TYPE = {
+    STUDENT: "Student",
+    INSTRUCTOR: "instructor",
+    ADMIN: "admin",
+}
+
+export const COURSE_STATUS = {
+    DRAFT: "draft",
+    PUBLISHED: "published"
+}
