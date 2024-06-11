f6121f10d64b002fe64f0203353f1d10cae62cd3
Anandita Singh <ananditasingh544@gmail.com>
2024-06-11 15:41:45 +0530

    jhgffg

--- SERVER/routes/Contact.js
+++ SERVER/routes/Contact.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { contactUsController } = require("../controllers/ContactUs");
+const { contactUsController } = require("../controllers/contactUs");
 const router = express.Router();
 
 router.post("/contact", contactUsController);
